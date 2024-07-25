d6ebebc829567fb3091044711cbd88aa61f940e5
Anas Sheikh Salem <anas.sheikh.salem@outlook.de>
2024-07-25 13:08:21 +0200

    Replacing with Next-Auth

---
+++ src/app/auth/[...nextauth]/route.js
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    CredentialsProvider({
+    name: "Credentials",
+    credentials: {
+      email: { label: "Email", type: "email", placeholder: "email@email.com" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      // Add logic here to look up the user from the credentials supplied
+      const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+      if (user) {
+        // Any object returned will be saved in `user` property of the JWT
+        return user
+      } else {
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      }
+    }
+  })
+  ],
+}
+
+export default NextAuth(authOptions)
